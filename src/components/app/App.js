ae78016084d7d8594843d3fe8a526f8a0a704f87
kusik16 <slobodyanuck@gmail.com>
2022-09-15 12:13:25 +0300

    delete constructor, add form in form, add static propTypes, add ContactListItem

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -7,18 +7,15 @@ import ContactList from '../contactList/ContactList';
 
 import app from './App.module.css';
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      contacts: [
-        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      ],
-      filter: '',
-    };
-  }
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
 
   onFilter = e => {
     this.setState(() => {
@@ -28,7 +25,8 @@ class App extends Component {
     });
   };
 
-  onAddContact = (name, number) => {
+  onAddContact = (e, name, number) => {
+    e.preventDefault();
     if (
       this.state.contacts.filter(contact => contact.name === name).length >= 1
     ) {
@@ -68,13 +66,11 @@ class App extends Component {
   render() {
     const { contacts, filter } = this.state;
 
-    const filteredContacts = !filter
-      ? contacts
-      : contacts.filter(
-          contact =>
-            contact.name.toLowerCase().includes(filter.toLowerCase()) ||
-            (contact.number + '').includes(filter)
-        );
+    const filteredContacts = contacts.filter(
+      contact =>
+        contact.name.toLowerCase().includes(filter.toLowerCase()) ||
+        (contact.number + '').includes(filter)
+    );
 
     return (
       <div>
